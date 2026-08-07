b3cd3cdb01fa43c840b8b6505db866250f5cbf20
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2026-08-07 22:02:54 +0700

    perf: import only used Manrope weights

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,8 @@
-import {
-  Manrope_500Medium,
-  Manrope_600SemiBold,
-  Manrope_700Bold,
-  Manrope_800ExtraBold,
-  useFonts,
-} from '@expo-google-fonts/manrope';
+import { Manrope_500Medium } from '@expo-google-fonts/manrope/500Medium';
+import { Manrope_600SemiBold } from '@expo-google-fonts/manrope/600SemiBold';
+import { Manrope_700Bold } from '@expo-google-fonts/manrope/700Bold';
+import { Manrope_800ExtraBold } from '@expo-google-fonts/manrope/800ExtraBold';
+import { useFonts } from '@expo-google-fonts/manrope/useFonts';
 import * as SplashScreen from 'expo-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from '@/design-system/theme/ThemeProvider';
