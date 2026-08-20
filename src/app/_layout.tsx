281feffb4d1e18fb24f02d1c9acfa0c2c02d6d3f
BlizzardBlast <fdsurya7@gmail.com>
2026-08-20 15:44:16 +0700

    feat: initial sentry setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react-native';
 import type { ErrorBoundaryProps } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StrictMode } from 'react';
@@ -13,6 +14,28 @@ import { RootNavigator } from '@/navigation/RootNavigator';
 import { configureNotificationHandling } from '@/platform/notifications/weeklyReflection';
 import { RootErrorScreen } from '@/shared/components/RootErrorScreen';
 import { ignoreError } from '@/shared/utils/function';
+
+Sentry.init({
+  dsn: 'https://14ee0c9c15f1270cd72c729104c5df0c@o4511942233227264.ingest.us.sentry.io/4511942238666752',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 SplashScreen.preventAutoHideAsync().catch(ignoreError);
 configureNotificationHandling().catch(ignoreError);
